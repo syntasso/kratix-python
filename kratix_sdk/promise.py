4fc1bd03d21406c6d46ea71d2ddd357242ab2bc5
Chunyi Lyu <chunyineko@gmail.com>
2025-08-11 17:02:46 +0100

    docs: add descriptions for resource and promise functions

--- kratix_sdk/promise.py
+++ kratix_sdk/promise.py
@@ -6,10 +6,13 @@ class Promise:
         self.data: Dict[str, Any] = data or {}
 
     def get_name(self) -> str:
+        """Get the name of the promise."""
         return self.data.get("metadata", {}).get("name", "")
 
     def get_labels(self) -> Dict[str, str]:
+        """Get the labels of the promise."""
         return self.data.get("metadata", {}).get("labels", {}) or {}
 
     def get_annotations(self) -> Dict[str, str]:
+        """Get the annotations of the promise."""
         return self.data.get("metadata", {}).get("annotations", {}) or {}
--- kratix_sdk/resource.py
+++ kratix_sdk/resource.py
@@ -10,9 +10,13 @@ class Resource:
         self.data = data
 
     def get_value(self, path: str) -> Any:
+        """Get a value from the resource request by path."""
         return _get_by_path(self.data, path)
 
     def get_status(self, path: str = "") -> Status:
+        """Get the status of the resource by path.
+        If path is empty, return the entire status.
+        If path is provided, return the value at that path."""
         status_data = self.data.get("status", {})
         if path:
             value = _get_by_path(status_data, path)
@@ -20,12 +24,15 @@ class Resource:
         return Status(status_data)
 
     def get_name(self) -> str:
+        """Get the name of the resource."""
         return self.data.get("metadata", {}).get("name", "")
 
     def get_namespace(self) -> str:
+        """Get the namespace of the resource."""
         return self.data.get("metadata", {}).get("namespace", "")
 
     def get_group_version_kind(self) -> GroupVersionKind:
+        """Get the GroupVersionKind of the resource."""
         api_version = self.data.get("apiVersion", "")
         if "/" in api_version:
             group, version = api_version.split("/", 1)
@@ -35,7 +42,9 @@ class Resource:
         return GroupVersionKind(group=group, version=version, kind=kind)
 
     def get_labels(self) -> Dict[str, str]:
+        """Get the labels of the resource."""
         return self.data.get("metadata", {}).get("labels", {})
 
     def get_annotations(self) -> Dict[str, str]:
+        """Get the annotations of the resource."""
         return self.data.get("metadata", {}).get("annotations", {})
