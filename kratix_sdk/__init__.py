370a2f9d9b74aacefa745ad060ebaa5adc61b49e
Chunyi Lyu <chunyineko@gmail.com>
2025-08-11 14:54:04 +0100

    chore: explicitly define public api
    - to make linter happy

--- kratix_sdk/__init__.py
+++ kratix_sdk/__init__.py
@@ -11,3 +11,18 @@ from .kratix_sdk import (
 )
 from .promise import Promise
 from .types import GroupVersionKind, DestinationSelector
+
+__all__ = [
+    "Status",
+    "Resource",
+    "Promise",
+    "GroupVersionKind",
+    "DestinationSelector",
+    "KratixSDK",
+    "set_input_dir",
+    "set_output_dir",
+    "get_input_dir",
+    "get_output_dir",
+    "get_metadata_dir",
+    "set_metadata_dir",
+]
